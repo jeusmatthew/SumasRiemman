995789699bd1e9b45593aa5905123a8fc75d5b5b
jeusmateo <jeusmateo@gmail.com>
2023-09-20 14:53:35 -0600

    chn

--- sumasRiemann.js
+++ sumasRiemann.js
@@ -143,6 +143,67 @@ function resolverParticionRegular() {
   });
 }
 
+function resolverParticionIrregular() {
+  let a = parseFloat(document.getElementById("a").value);
+  let b = parseFloat(document.getElementById("b").value);
+  let n = parseInt(document.getElementById("n").value);
+
+  let particiones = [];
+
+  // Generando particiones
+  for (let i = 0; i <= n; i++) {
+    particiones.push(randomIntFromInterval(a, b));
+  }
+
+  particiones.sort();
+
+  let tipoMuestras = document.getElementsByName("sample-point");
+
+  let resultado = 0;
+  let format = `$$ \\int_{${a}}^{${b}} x^{${LONGITUD_NOMBRE}}+1 = `;
+
+  let idElemento = "";
+  tipoMuestras.forEach((element) => {
+    if (element.checked) {
+      switch (element.value) {
+        case "derecho":
+          resultado = sumasRiemannDerecha(a, b, n);
+          idElemento = "outDerecha";
+          break;
+        case "izquierdo":
+          resultado = sumasRiemannIzquierda(a, b, n);
+          idElemento = "outIzquierda";
+          break;
+        case "medio":
+          resultado = sumasRiemannMedio(a, b, n);
+          idElemento = "outMedio";
+          break;
+        case "azar":
+          resultado = sumasRiemannPuntoAzar(a, b, n);
+          idElemento = "outAzar";
+          break;
+      }
+      imprimirTeX(
+        document.getElementById(idElemento),
+        format.concat(`${resultado} $$`)
+      );
+    } else {
+      const output = document.getElementById("output").children;
+      switch (element.value) {
+        case "derecho":
+          output[0].innerHTML = "";
+        case "izquierdo":
+          output[1].innerHTML = "";
+        case "medio":
+          output[2].innerHTML = "";
+        case "azar":
+          output[3].innerHTML = "";
+      }
+    }
+  });
+
+}
+
 function imprimirTeX(element = HTMLElement, str = "") {
   MathJax.typesetPromise()
     .then(() => {
@@ -150,4 +211,13 @@ function imprimirTeX(element = HTMLElement, str = "") {
       MathJax.typesetPromise();
     })
     .catch((err) => console.log(err.message));
+}
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return (Math.random() * (max - min) + min).toFixed(4);
+}
+
+function sumaRiemannIrregular(){
+  
 }
